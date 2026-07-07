87f844d093a78705907a1c6ffb431736c64b519c
Rahul Verma <rahulverma@Rahuls-MacBook-Air.local>
2026-07-07 16:22:36 +0530

    created a websocket server

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 import https from "https";
+import { WebSocketServer} from "ws";
 
 const PORT = process.env.PORT || 9000;
 
 const httpsServer = https.createServer(async function(req, res){})
+const wsServer = new WebSocketServer({ server: httpsServer });
+
+wsServer.on("connection", (socket) => {
+  console.log("Client connected");
+
+  socket.on("message", (message) => {
+    console.log(`Received message: ${message}`);
+
+    socket.send(`Server received: ${message}`);
+  });
+
+  socket.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
 
 httpsServer.listen(PORT, () => {
   console.log(`HTTPS Server running on port ${PORT}`);
